aabe2840802fc53ae21aa0ff1ff7d814f2d42a5e
OpenShift guest <matt.halbe@va.gov>
2013-07-11 15:31:46 -0400

    tweak

--- feedstrap/reports.py
+++ feedstrap/reports.py
@@ -19,11 +19,11 @@ def get_rating(val, factor):
                 'relevance': (4, 10),
                 'impact':(3, 8),}
     if val >= 0:
-        button = '<span class="label">1-LOW</span>'
+        button = '<span style="color:white">1</span><span class="label">LOW</span>'
     if val >= rate_dic[factor][0]:
-        button = '<span class="label label-warning">2-MED</span>'
+        button = '<span style="color:white">2</span><span class="label label-warning">MED</span>'
     if val >= rate_dic[factor][1]:
-        button = '<span class="label label-important">3-HIGH</span>'
+        button = '<span style="color:white">3</span><span class="label label-important">HIGH</span>'
     return button
 
 def esil(request,  site="vacloud.us"):
