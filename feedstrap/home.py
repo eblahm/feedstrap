7cad3c047e11c1969d8de853c809383acde6bf26
OpenShift guest <matt.halbe@va.gov>
2013-10-04 13:52:56 -0400

    simple searches working, still need to work out javascript widgets and full text searches

--- feedstrap/home.py
+++ feedstrap/home.py
@@ -1,6 +1,8 @@
 from filter import apply_filter, generate_filter_tags
 import render
 from models import StaticPage as StaticPageModel
+from search import AdvancedSearch
+from config import per_page_limit
 
 
 def MainPage(request, template=""):
@@ -14,11 +16,33 @@ def MainPage(request, template=""):
         if alert_query.count() > 0:
             v['alert'] = alert_query.get().content
 
-    # load all the data pertaining to database queries
-    v.update(apply_filter(request))
-
-    # make sure the templates are aware of get perameters
-    v.update(filter_conditions)
+    # load info pertaining to database queries
+    class pageinfo(AdvancedSearch): pass
+    PG = pageinfo()
+    v['results'] = PG.get_results(request.GET)
+    v['total'] = len(v['results'])
+    v['search'] = PG
+    
+     
+    # where are we in terms of offset?
+    try:
+        start_offset = int(request.GET.get('s', 0))
+    except:
+        start_offset = 0
+        
+    # fetch db data according to offset and per page limit
+    limit = start_offset + per_page_limit
+    v['results'] = v['results'][start_offset:limit]
+    
+    # only allow for "show more" if there are actually more to show
+    if v['total'] >= (limit + 1):
+        get_request_copy = request.GET.copy()
+        get_request_copy.__setitem__('s', limit)
+        v['show_more_perams'] = get_request_copy.urlencode()
+    elif v['total'] > 0:
+        v['end_info'] = 'End of Search Results'
+    else:
+        v['end_info'] = 'No Results Found for that Search'
 
     # has the user landed at the page or just clicked "Show More"?
     if template == "ajax":
@@ -26,7 +50,6 @@ def MainPage(request, template=""):
     else:
         template_file = 'main/home.html'
         v['get_query'] = request.GET.urlencode()
-        v['filter_tags'] = generate_filter_tags(request)
 
     return render.response(request, template_file, v)
 
--- feedstrap/search.py
+++ feedstrap/search.py
@@ -5,139 +5,49 @@ from django.contrib.auth.models import User
 from django import forms
 from django.db.models import Q
 
-from models import Resource, Tag, Report, Office, Feed, Topic
+from models import *
+import models
 
 
 class Filter():
-    def __init__(self):
-        self._target_model = Resource
-        self.pk_search = False
-        self.name = ''
-        self.color = 'black'
-        self.condition_model = Resource
-        self.query_expression = ''
-        self.form_element = forms.CharField()
-        self.inverse = ""
+    form_element = forms.CharField()
+    name = ''
+    color = 'black'
+    
+    condition_model = Resource
+    query_expression = ''
+    pk_search = False
+    display_value = None
+    inverse = ""
 
-    def _pk_to_name(model, pk):
+    def _pk_to_name(self, condition_model, pk):
         """
         as an alternative to the standard display_value method
         and in order to make it more clear for the user of advanced search widget
         return name of underlying item assoicated with the selected pk
         """
-        q_by_pk = model.objects.filter(pk=pk)
+        
+        q_by_pk = condition_model.objects.filter(pk=pk)
         if q_by_pk:
             return q_by_pk.get().name
         else:
             return ''
-
+            
+    def _get_display_value(self, queried_value):
+        """
+        return a string representing search condition
+        """
+        
+        if self.pk_search:
+            return self._pk_to_name(self.condition_model, queried_value)
+        else:
+            return queried_value
     def process_string_input(self, sinput):
         if isinstance(sinput, unicode):
             return sinput.encode('utf-8')
         else:
             return str(sinput)
 
-    def display_value(self, queried_value):
-        """
-        return a string representing search condition
-        """
-        if self.pk_search:
-            return self._pk_to_name(self.condition_model, queried_value)
-        else:
-            return queried_value
-
-
-class OR_Statement(Filter):
-    # for display purposes only
-    name = "OR"
-    color = "black"
-
-class AdvancedSearch():
-    def __init__(self, query):
-        # pending "OR" statments between query expressions
-        # subsets must be generated, then "OR"-ed together
-        self._pending_queries = [ [] ] # only one level of subset is allowed
-
-        # multiple conditions are restrictive and continually limiting by default, unless specified with "OR"
-        self._and_statement = True
-        self.registered_filters = {}
-
-        self.query = query
-        self.applied_filters = []
-
-    def register_filters(self, filters):
-        for f in filters:
-            self.registered_filters[f.name] = f
-
-    def _apply(self, name, condition, inverse_paramters):
-        if condition == 'OR' and name == "ANDOR":
-            # or statments are passed like conditions and provide information about what to do with the next condition
-            # in this event modify _and_statment so the next condition passed to apply() will be included in a new subset
-            self._and_statement = False
-            thisOR = OR_Statement()
-            thisOR.inverse_get_parameters = inverse_paramters
-            self.applied_filters.append(thisOR)
-            return self
-
-        filter = self.registered_filters.get(name, None)
-        if not filter:
-            return self
-            
-        filter = filter()
-        q = Q(filter.query_expression, filter.process_string_input(condition))
-
-        # django Q() allows for &,|
-        if self._and_statement:
-            # append this query to the most recent subset
-            self._pending_queries[-1].append(q)
-        else:
-            # create a new subset using this query as first item
-            self._pending_queries.append([q])
-            # any subsequent queryies will be treated as a part of this subset until "OR" appears again
-            self._and_statement = True
-        filter.inverse = inverse_paramters
-        self.applied_filters.append(filter)
-        return self
-
-    def get_results(self, get_parameters):
-
-        # apply all the filters, in order
-        # get perams should look like {"1_tags": "Health Care", "2_reports": "weekly reads"}
-        sorted_params = sorted(get_parameters.iteritems(), key=operator.itemgetter(0)) 
-        for sp in sorted_params:
-            name = sp.split("_")[-1] # ex. 1_tags >> tags
-            condition = sorted_params[sp] # ex. Health Care
-            
-            # the inverse get parameters allow for the X click on each filter tag
-            inverse_get = get_parameters.copy()
-            del inverse_get[sp]
-            self._apply(name, condition, inverse_get.urlencode())
-
-        # either "OR" them or "AND" them together
-        processed_subsets = []
-        for subset in self._pending_queries:
-            # actually fetch search results for each query expression in the subset
-            # use first item in list as the base for other queries
-            ps = [self._target_model.objects.filter(subset[0])]
-            # if more than one query
-            # they each should be "AND"-ed together
-            for q in subset[1:]:
-                ps[0] = ps[0] & self._target_model.objects.filter(q) 
-                
-            processed_subsets.append(ps)
-
-        if len(processed_subsets) > 1:
-            # subsets should be "OR" ed together
-            results = []
-            for ps in processed_subsets:
-                results | ps
-            return results.distinct().order_by('-date')
-        else:
-            # No "OR" statments were found
-            return processed_subsets[0].order_by('-date')
-
-
-    
 class TagsFilter(Filter):
     name = 'tags'
     color = '#0088CC'
@@ -150,7 +60,7 @@ class PersonFilter(Filter):
     color = 'orange'
     query_expression = 'feeds__user__first_name'
     condition_model = Feed
-    form_element = forms.ChoiceField(choices=models.generate_choices(User, 'first_name', 'first_name'))
+    form_element = forms.ChoiceField(choices=generate_choices(User, 'first_name', 'first_name'))
 
 class FeedsFilter(Filter):
     name = 'feeds'
@@ -158,7 +68,7 @@ class FeedsFilter(Filter):
     query_expression = 'feeds__pk'
     condition_model = Feed
     pk_search = True
-    form_element = forms.ChoiceField(choices=models.generate_choices(models.Feed))
+    form_element = forms.ChoiceField(choices=generate_choices(Feed))
 
 class ESILFilter(Filter):
     name = 'ESIL'
@@ -166,14 +76,14 @@ class ESILFilter(Filter):
     query_expression = 'topics__pk'
     condition_model = Topic
     pk_search = True
-    form_element = forms.ChoiceField(choices=models.generate_choices(models.Topic))
+    form_element = forms.ChoiceField(choices=generate_choices(Topic))
 
 class ReportFilter(Filter):
-    name = 'Report'
+    name = 'report'
     color = '#2D6987'
     query_expression = 'reports__name'
     condition_model = Report
-    form_element = forms.ChoiceField(choices=models.generate_choices(models.Report, 'name', 'name'))
+    form_element = forms.ChoiceField(choices=generate_choices(Report, 'name', 'name'))
 
 class DateToFilter(Filter):
     name = 'Date To'
@@ -192,8 +102,99 @@ class OfficeFilter(Filter):
     color = 'green'
     query_expression = 'offices__name'
     condition_model = Office
-    form_element = forms.ChoiceField(choices=models.generate_choices(models.Office,'name', 'name'))
+    form_element = forms.ChoiceField(choices=generate_choices(Office,'name', 'name'))
 
-AS = AdvancedSearch()
-AS.register_filters([TagsFilter, PersonFilter, FeedsFilter, ESILFilter,
-                     ReportFilter, DateToFilter, DateFromFilter, OfficeFilter])
+
+registered = [TagsFilter, PersonFilter, FeedsFilter, ESILFilter,
+                     ReportFilter, DateToFilter, DateFromFilter, OfficeFilter]
+
+class OR_Statement(Filter):
+    # for display purposes only
+    name = "OR"
+    color = "black"
+
+class AdvancedSearch():
+    def __init__(self, registered=registered):
+        # pending "OR" statments between query expressions
+        # subsets must be generated, then "OR"-ed together
+        self._pending_queries = [ [] ] # only one level of subset is allowed
+
+        # multiple conditions are restrictive and continually limiting by default, unless specified with "OR"
+        self._and_statement = True
+        self.registered_filters = {}
+        for f in registered:
+            self.registered_filters[f.name] = f
+        
+        self._base_query = Resource.objects.all()
+        self.applied_filters = []
+
+    def _apply(self, name, condition, inverse_paramters):
+        if condition == 'OR' and name == "ANDOR":
+            # or statments are passed like conditions and provide information about what to do with the next condition
+            # in this event modify _and_statment so the next condition passed to apply() will be included in a new subset
+            self._and_statement = False
+            thisOR = OR_Statement()
+            thisOR.inverse_get_parameters = inverse_paramters
+            self.applied_filters.append(thisOR)
+            return None
+
+        filter = self.registered_filters.get(name, False)
+        if filter: 
+            filter = filter()
+            exp = filter.query_expression
+            formated_condition = filter.process_string_input(condition)
+            
+            q = Q((exp, formated_condition))
+    
+            # django Q() allows for &,|
+            if self._and_statement:
+                # append this query to the most recent subset
+                self._pending_queries[-1].append(q)
+            else:
+                # create a new subset using this query as first item
+                self._pending_queries.append([q])
+                # any subsequent queryies will be treated as a part of this subset until "OR" appears again
+                self._and_statement = True
+                
+            filter.display_value = filter._get_display_value(condition)
+            filter.inverse = inverse_paramters
+            self.applied_filters.append(filter)
+            return None
+
+    def get_results(self, get_parameters):
+
+        # apply all the filters, in order
+        # get perams should look like {"1_tags": "Health Care", "2_reports": "weekly reads"}
+        sorted_params = sorted(get_parameters.iteritems(), key=operator.itemgetter(0)) 
+        for condition_name, condition in sorted_params:
+            filter_name = condition_name.split("_")[-1] # ex. 1_tags >> tags
+            
+            # the inverse get parameters allow for the X click on each filter tag
+            inverse_get = get_parameters.copy()
+            del inverse_get[condition_name]
+            self._apply(filter_name, condition, inverse_get.urlencode())
+
+        # actually construct query object using query expressions in the subset
+        processed_subsets = []
+        for subset in self._pending_queries:
+            if len(subset) > 0:
+                # use first item in list as the base for other queries
+                base = Resource.objects.all().filter(subset[0])
+                # if more than one query
+                # they each should be "AND"-ed together
+                for q in subset[1:]:
+                    base &= base.filter(q)
+                processed_subsets.append(base)
+                    
+        if len(processed_subsets) == 0:
+            # no valid query expressions
+            return self._base_query.order_by('-date')
+        elif len(processed_subsets) == 1:
+            # no "OR" statments found, only one subset available
+            return processed_subsets[0].order_by('-date')
+        else:
+            # subsets should be "OR" ed together
+            results = []
+            for ps in processed_subsets:
+                results | ps
+            return results.distinct().order_by('-date')
