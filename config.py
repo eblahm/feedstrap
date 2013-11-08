fbbb2fff52ddb80c776800f90b9a7c800362ddbe
keisele <kathleen.eisele@va.gov>
2013-11-08 10:38:45 -0500

    Enabling Solr

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 app_root = os.path.abspath(os.path.split(os.path.dirname(__file__))[0])
 
 # is the Solr Server configured for full text search?
-solr_enabled = False
+solr_enabled = True
 
 # how many search results per page
 per_page_limit = 10
